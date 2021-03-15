8b3b34e9751d345dfdda92e93d65781ce616634a
Ruth-Ann Armstrong <ruthanna@stanford.edu>
2021-03-14 17:18:33 -0700

    experimenting with gen

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ use rand::{thread_rng, Rng};
 static GROUP_SIZE: u64 = 65536;
 static GROUP_SIZE_SIGNED: i64 = 65536;
 
-static SEC_PARAM: i64 = 6;
+static SEC_PARAM: i64 = 4;
 
 fn main() {
 
@@ -36,15 +36,15 @@ fn main() {
     // println!("1 1 1 1{:?}", 1 ^ 1 ^ 1 ^ 1);
     println!("GENERATE");
 
-    let result = gen(6, 12, 1);
-    // println!("{:?}", result[0]);
-    // println!("{:?}", result[1]);
+    let result = gen(4, 7, 1);
+    println!("{:?}", result[0]);
+    println!("{:?}", result[1]);
     let k_0 = &result[0];
     let k_1 = &result[1];
     println!("EVAL");
 
-    let check_0 = eval(0, k_0.to_string(), 11, 6);
-    let check_1 = eval(1, k_1.to_string(), 11, 6);
+    let check_0 = eval(0, k_0.to_string(), 7, 4);
+    let check_1 = eval(1, k_1.to_string(), 7, 4);
 
     // println!("CHECK 0 {:?}", check_0);
     // println!("CHECK 1 {:?}", check_1);
@@ -94,14 +94,15 @@ fn eval(b: u32, k_b: String, x:i64, sec_param:usize)  -> i64{
         let mut ra = ChaChaRng::new_unseeded();
 
         ra.set_stream(s_prev.into());
-        let gen = ra.next_u32();
+      //  let gen = ra.next_u32();
+        let gen = s_prev;
         let parse_concat = u32::from_str_radix(&format!("{}{}{}{}", s_cw, t_l_cw, s_cw, t_r_cw), 2).unwrap();
         let tau_i = gen ^ (t_prev * parse_concat);
         println!("RUN {:?}", i);
 
         //parse tau
-        let tau_parse_str = format!("{:b}", tau_i);
-        println!("TAU {:?}", tau_parse_str);
+        let tau_parse_str = pad_bits(format!("{:b}", tau_i), (2 * sec_param) + 2);
+        println!("TAU {:?}", tau_parse_str.len());
 
         let s_l = u32::from_str_radix(&tau_parse_str[0..sec_param], 2).unwrap();
         let t_l = u32::from_str_radix(&tau_parse_str[sec_param..sec_param+1], 2).unwrap();
@@ -131,11 +132,20 @@ fn eval(b: u32, k_b: String, x:i64, sec_param:usize)  -> i64{
     return return_;
 }
 
+// fn prng(n: u64) -> u64{
+//     let mut prng = ChaChaRng::new_unseeded();
+//     prng.set_stream(n);
+//     let gen:u64 = prng.next_u32().into();
+//     return gen;
+// }
+
 
 fn convert(s: u64) -> i64{
     let mut prng = ChaChaRng::new_unseeded();
     prng.set_stream(s);
     let gen_s:u64 = prng.next_u32().into();
+    let gen_s:u64 = s;
+
     let return_;
     let result = power_of_two(GROUP_SIZE);
  
@@ -155,9 +165,9 @@ fn convert(s: u64) -> i64{
         return_ = gen_s % GROUP_SIZE;
     }
     let signed = return_ as i64;
-    // println!("UNSIGNED CONVERT {:?}", return_);
+    println!("UNSIGNED CONVERT {:?}", return_);
 
-    // println!("CONVERT {:?}", signed);
+    println!("CONVERT {:?}", signed);
     return signed;
 }
 
@@ -191,8 +201,12 @@ fn pad_bits(s:String, len:usize) -> String{// Yes
     let padded;
     if  s.len() < len{
         let extension = String::from_utf8(vec![b'0'; len - s.len()]).unwrap();
+        // println!("S LENGTH  if {:?}  {:?}", len, s.len());
+
         padded = format!("{}{}", extension, s);
     }else{
+        // println!("S LENGTH  else {:?}  {:?}", len, s.len());
+
         padded = format!("{}", s);
     }
     return padded;
@@ -201,12 +215,12 @@ fn pad_bits(s:String, len:usize) -> String{// Yes
 
 fn gen(sec_param: usize , alpha:i64, beta:i64) -> Vec<String>{
 
-    //bit decomposition of alpha
-    let alpha_vec: Vec<char> = format!("{:b}", alpha).chars().collect();
-    let n = alpha_vec.len();
-    println!("N LENGTH {:?}", n);
+    //bit decomposition of alpha -yes
+    let alpha_vec: Vec<char> = format!("{:b}", alpha).chars().collect();//yes
+    let n = alpha_vec.len();//yes
+    // println!("alpha {:?} {:?} n {:?}", alpha, alpha_vec, n);
 
-    //randomly sample s_0 and s_1
+    //randomly sample s_0 and s_1 - yes line 2
     let mut rng = rand::thread_rng();
     let mut s_0:u32 = 0;
     let mut s_1:u32 = 0;
@@ -215,19 +229,21 @@ fn gen(sec_param: usize , alpha:i64, beta:i64) -> Vec<String>{
         s_1 = (s_1 << 1)  | rng.gen_range(0,2);
 
     }
-    // s_0 = 25;
-    // s_1 = 35;
-    let mut s0_prev = s_0;
+    // println!("s_0 {:?} s_1 {:?}", s_0, s_1);
+ 
+    s_0 = 5;
+    s_1 = 7;
+    let mut s0_prev = s_0; //unchecked
     let mut s1_prev = s_1;
 
     //begin building k
     let mut k_0 = pad_bits(format!("{:b}", s_0), sec_param);
     let mut k_1 = pad_bits(format!("{:b}", s_1), sec_param);//correct length
-    println!("K_0 init {:?}", k_0);
-    println!("K_1 init {:?}", k_1);
+    // println!("K_0 init {:?}", k_0);
+    // println!("K_1 init {:?}", k_1); //yes
 
 
-    //initialize t-bits
+    //initialize t-bits - yes
     let mut t0_prev:u32 = 0;
     let mut t1_prev:u32 = 1;
     println!("___________________________");
@@ -237,13 +253,14 @@ fn gen(sec_param: usize , alpha:i64, beta:i64) -> Vec<String>{
         //generate based on previous s0
         let mut prng_0 = ChaChaRng::new_unseeded();
         prng_0.set_stream(s0_prev.into());
-        let gen_s0 = prng_0.next_u32();
-        println!("RUN {:?}", i);
+        // let gen_s0 = prng_0.next_u32();
+        let gen_s0 = s0_prev;
 
-        println!("GEN S_0 {:?}", format!("{:b}",gen_s0));
+        println!("RUN {:?}", i);
+        println!("S_0 {:?} GEN S_0 {:?}",  format!("{:b}",s_0), format!("{:b}",gen_s0));
      
         //parse prng output
-        let gen_s0_bits = format!("{:b}", gen_s0);
+        let gen_s0_bits = format!("0101010101010{:b}", gen_s0);
         let s0_l = u32::from_str_radix(&gen_s0_bits[0..sec_param], 2).unwrap();
         let t0_l = u32::from_str_radix(&gen_s0_bits[sec_param..sec_param+1], 2).unwrap();
         let s0_r =  u32::from_str_radix(&gen_s0_bits[sec_param+1..2*sec_param + 1], 2).unwrap();
@@ -271,11 +288,12 @@ fn gen(sec_param: usize , alpha:i64, beta:i64) -> Vec<String>{
         //generate based on previous s1
         let mut prng_1 = ChaChaRng::new_unseeded();
         prng_1.set_stream(s1_prev.into());
-        let mut gen_s1 = prng_1.next_u32();
+        let mut gen_s1 = s1_prev;
+       // let mut gen_s1 = prng_1.next_u32();
         // println!("GEN S_1 {:?}", format!("{:b}",gen_s0));
 
         //parse prng output
-        let gen_s1_bits = format!("{:b}", gen_s1);
+        let gen_s1_bits = format!("0101010101010{:b}", gen_s1);
         let s1_l = u32::from_str_radix(&gen_s1_bits[0..sec_param], 2).unwrap();
         let t1_l = u32::from_str_radix(&gen_s1_bits[sec_param..sec_param+1], 2).unwrap();
         let s1_r =  u32::from_str_radix(&gen_s1_bits[sec_param+1..2*sec_param + 1], 2).unwrap();
@@ -295,9 +313,11 @@ fn gen(sec_param: usize , alpha:i64, beta:i64) -> Vec<String>{
             t1_lose = t1_l;
         }
 
-        let s_cw = s0_lose ^ s1_lose;
+        let s_cw = s0_lose ^ s1_lose; //yes
         let t_cw_l = t0_l ^ t1_l ^ alpha_i ^ 1;
         let t_cw_r = t0_r ^ t1_r ^ alpha_i;
+        // println!("S0 lose {:?} S1 lose {:?} Scw  {:?}", s0_lose, s1_lose, s_cw);
+        println!("t_cw_l {:?} t_cw_r {:?} t0_l  {:?} t1_l  {:?}", t_cw_l, t_cw_r, t0_r, t0_l);
 
         let cw_i = format!("{}{}{}", pad_bits(format!("{:b}", s_cw), sec_param), format!("{:b}", t_cw_l), format!("{:b}", t_cw_r));
         //  println!("CW N {:?}", cw_i.len());
@@ -309,10 +329,12 @@ fn gen(sec_param: usize , alpha:i64, beta:i64) -> Vec<String>{
         let t1_i;
 
         if keep == 'l'{
+            println!("KEEP L");
             t0_i = t0_keep ^ (t0_prev * t_cw_l);
             t1_i = t1_keep ^ (t1_prev * t_cw_l);
 
         }else{
+            
             t0_i = t0_keep ^ (t0_prev * t_cw_r);
             t1_i = t1_keep ^ (t1_prev * t_cw_r);
         }
